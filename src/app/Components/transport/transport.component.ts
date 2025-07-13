6071f05bad37e93a708be467dc7c5b6cbb43d6fa
AkterruzzamanEPS <md.akterruzzaman@eps.com.bd>
2025-07-13 12:01:00 +0600

    Transport integration

--- src/app/Components/transport/transport.component.ts
+++ src/app/Components/transport/transport.component.ts
@@ -5,15 +5,12 @@ import { RouterModule, ActivatedRoute, Router } from '@angular/router';
 import { AgGridAngular } from 'ag-grid-angular';
 import { NgxEditorModule, Editor, Toolbar } from 'ngx-editor';
 import { ToastrService } from 'ngx-toastr';
-import { DistrictFilterDto } from '../../Models/RequestDto/District';
-import { ThanaFilterDto } from '../../Models/RequestDto/Thana';
-import { TouristZoneFilterDto } from '../../Models/RequestDto/TouristZone';
 import { PaginationComponent } from '../../Shared/pagination/pagination.component';
 import { AGGridHelper } from '../../Shared/Services/AGGridHelper';
 import { AuthService } from '../../Shared/Services/auth.service';
 import { CommonHelper } from '../../Shared/Services/CommonHelper';
 import { HttpHelperService } from '../../Shared/Services/http-helper.service';
-import { TransportFilterDto, TransportRequestDto } from '../../Models/RequestDto/Transport';
+import { EndingPointFilterDto, StartingPointFilterDto, TransportFilterDto, TransportRequestDto } from '../../Models/RequestDto/Transport';
 
 @Component({
   selector: 'app-transport',
@@ -24,27 +21,20 @@ import { TransportFilterDto, TransportRequestDto } from '../../Models/RequestDto
   providers: [DatePipe],
   encapsulation: ViewEncapsulation.None,
 })
-export class TransportComponent implements OnInit {
+export class TransportComponent  implements OnInit {
 
   private transportGridApi!: any;
   public DeafultCol = AGGridHelper.DeafultCol;
   public rowData!: any[];
-  public districtList: any[] = [];
-  public districtFromList: any[] = [];
-  public thanaList: any[] = [];
-  public thanaFromList: any[] = [];
-  public touristZoneList: any[] = [];
-  public touristZoneFromList: any[] = [];
-
-  public oThanaFilterDto = new ThanaFilterDto();
-  public oThanaFilterDtoFrom = new ThanaFilterDto();
-  public oDistrictFilterDto = new DistrictFilterDto();
-
-  public oTouristZoneFilterDto = new TouristZoneFilterDto();
-
+  public startingpointList: any[] = [];
+  public startingpointFromList: any[] = [];
+  public endingpointList: any[] = [];
+  public endingpointFromList: any[] = [];
+  public oEndingPointFilterDto = new EndingPointFilterDto();
+  public oEndingPointFilterDtoFrom = new EndingPointFilterDto();
+  public oStartingPointFilterDto = new StartingPointFilterDto();
   public oTransportFilterDto = new TransportFilterDto();
   public oTransportRequestDto = new TransportRequestDto();
-
   public transportId = 0;
   public editor: Editor = new Editor();
   public toolbar: Toolbar;
@@ -57,25 +47,29 @@ export class TransportComponent implements OnInit {
   public totalPageNumbers: number[] = [];
 
   public colDefsTransection: any[] = [
-   { valueGetter: "node.rowIndex + 1", headerName: 'SL', width: 70, editable: false },
-   { field: 'Name', width: 150, headerName: 'Transport Name', filter: true },
-   { field: 'PhoneNo', width: 120, headerName: 'Phone No', filter: true },
-   { field: 'Description', width: 180, headerName: 'Description', filter: true },
-   { field: 'Latitude', width: 120, headerName: 'Latitude', filter: true },
-   { field: 'Longitude', width: 120, headerName: 'Longitude', filter: true },
-   { field: 'StartingPoint', width: 150, headerName: 'Starting Point', filter: true },
-   { field: 'EndingPoint', width: 150, headerName: 'Ending Point', filter: true },
-   { field: 'IsActive', width: 100, headerName: 'Status', filter: true },
-   { field: 'Remarks', width: 200, headerName: 'Remarks', filter: true }
+    { valueGetter: "node.rowIndex + 1", headerName: 'SL', width: 90, editable: false, checkboxSelection: false },
+    { field: 'Name', width: 150, headerName: 'Transport Name', filter: true },
+    { field: 'PhoneNo', width: 150, headerName: 'PhoneNo', filter: true },
+    { field: 'Driver', width: 150, headerName: 'Driver', filter: true },
+    { field: 'Cost', width: 150, headerName: 'Cost', filter: true },
+    { field: 'Seat', width: 150, headerName: 'Seat', filter: true },
+    { field: 'Address', width: 150, headerName: 'Address', filter: true },
+    { field: 'StartTime', width: 150, headerName: 'StartTime', filter: true },
+    { field: 'EndTime', width: 150, headerName: 'EndTime', filter: true },
+    { field: 'StartingPoint', width: 150, headerName: 'StartingPoint', filter: true },
+    { field: 'EndingPoint', width: 150, headerName: 'EndingPoint', filter: true },
+    { field: 'Description', width: 150, headerName: 'Description', filter: true },
+    { field: 'Latitude', width: 150, headerName: 'Latitude', filter: true },
+    { field: 'Longitude', width: 150, headerName: 'Longitude', filter: true },
+    { field: 'Remarks', headerName: 'Remarks' },
+    { field: 'IsActive', headerName: 'Status' },
   ];
 
   trackByFn: TrackByFunction<any> | any;
-  trackByDistrict: TrackByFunction<any> | any;
-  trackByDistrictFrom: TrackByFunction<any> | any;
-  trackByThana: TrackByFunction<any> | any;
-  trackByThanaFrom: TrackByFunction<any> | any;
-  trackByTouristZone: TrackByFunction<any> | any;
-  trackByTouristZoneFrom: TrackByFunction<any> | any;
+  trackByStartingPoint: TrackByFunction<any> | any;
+  trackByStartingPointFrom: TrackByFunction<any> | any;
+  trackByEndingPoint: TrackByFunction<any> | any;
+  trackByEndingPointFrom: TrackByFunction<any> | any;
   constructor(
     public authService: AuthService,
     private toast: ToastrService,
@@ -92,9 +86,9 @@ export class TransportComponent implements OnInit {
     if (serviceId != null) {
       this.oTransportFilterDto.ServiceId = Number(serviceId);
     }
-    this.GetDistricts();
+    this.GetStartingPoints();
+    this.GetEndingPoints();
     this.GetTransport();
-    this.GttTouristZones();
   }
 
   onGridReadyTransection(params: any) {
@@ -114,19 +108,6 @@ export class TransportComponent implements OnInit {
   Filter() {
     this.GetTransport();
   }
-  private GttTouristZones() {
-    this.oTouristZoneFilterDto.IsActive = CommonHelper.booleanConvert(this.oTouristZoneFilterDto.IsActive);
-    this.http.Post(`TouristZone/GetAllTouristZones`, this.oTouristZoneFilterDto).subscribe(
-      (res: any) => {
-        this.touristZoneList = res;
-        this.touristZoneFromList = res;
-      },
-      (err) => {
-        this.toast.error(err.ErrorMessage, "Error!!", { progressBar: true });
-      }
-    );
-  }
-
 
   private GetTransport() {
     this.oTransportFilterDto.IsActive = CommonHelper.booleanConvert(this.oTransportFilterDto.IsActive);
@@ -151,14 +132,14 @@ export class TransportComponent implements OnInit {
   }
 
 
-  private GetDistricts() {
-    this.oDistrictFilterDto.IsActive = CommonHelper.booleanConvert(this.oDistrictFilterDto.IsActive);
-    this.oDistrictFilterDto.Type = Number(this.oDistrictFilterDto.Type);
+  private GetStartingPoints() {
+    this.oStartingPointFilterDto.ServiceId = Number(this.oTransportFilterDto.ServiceId);
+
     // After the hash is generated, proceed with the API call
-    this.http.Post(`DistrictThana/GetAllDistricts`, this.oDistrictFilterDto).subscribe(
+    this.http.Post(`Transport/GetTransportsFrom`, this.oStartingPointFilterDto).subscribe(
       (res: any) => {
-        this.districtList = res;
-        this.districtFromList = res;
+        this.startingpointList = res;
+        this.startingpointFromList = res;
       },
       (err) => {
         this.toast.error(err.ErrorMessage, "Error!!", { progressBar: true });
@@ -166,14 +147,13 @@ export class TransportComponent implements OnInit {
     );
   }
 
-  private GetThanas() {
-    this.oThanaFilterDto.IsActive = CommonHelper.booleanConvert(this.oThanaFilterDto.IsActive);
-    this.oThanaFilterDto.Type = Number(this.oThanaFilterDto.Type);
-    this.oThanaFilterDto.DistinctId = Number(this.oThanaFilterDto.DistinctId);
+  private GetEndingPoints() {
+    this.oEndingPointFilterDto.ServiceId = Number(this.oTransportFilterDto.ServiceId);
     // After the hash is generated, proceed with the API call
-    this.http.Post(`DistrictThana/GetAllThanas`, this.oThanaFilterDto).subscribe(
+    this.http.Post(`Transport/GetTransportsTo`, this.oEndingPointFilterDto).subscribe(
       (res: any) => {
-        this.thanaList = res;
+        this.endingpointList = res;
+        this.endingpointFromList = res;
       },
       (err) => {
         this.toast.error(err.ErrorMessage, "Error!!", { progressBar: true });
@@ -182,31 +162,6 @@ export class TransportComponent implements OnInit {
 
   }
 
-  private GetThanasFrom() {
-    this.oThanaFilterDtoFrom.IsActive = CommonHelper.booleanConvert(this.oThanaFilterDtoFrom.IsActive);
-    this.oThanaFilterDtoFrom.Type = Number(this.oThanaFilterDtoFrom.Type);
-    this.oThanaFilterDtoFrom.DistinctId = Number(this.oThanaFilterDtoFrom.DistinctId);
-    // After the hash is generated, proceed with the API call
-    this.http.Post(`DistrictThana/GetAllThanas`, this.oThanaFilterDtoFrom).subscribe(
-      (res: any) => {
-        this.thanaFromList = res;
-      },
-      (err) => {
-        this.toast.error(err.ErrorMessage, "Error!!", { progressBar: true });
-      }
-    );
-  }
-
-  public districtChange(event: any) {
-    this.oThanaFilterDto.DistinctId = Number(event.target.value);
-    this.thanaList = [];
-    this.GetThanas();
-  }
-  public districtChangeFrom(event: any) {
-    this.oThanaFilterDtoFrom.DistinctId = Number(event.target.value);
-    this.thanaFromList = [];
-    this.GetThanasFrom();
-  }
 
 
   public onFileChange(event: any): void {
@@ -235,35 +190,13 @@ export class TransportComponent implements OnInit {
       this.toast.warning("Please enter name", "Warning!!", { progressBar: true });
       return;
     }
-    if (this.oTransportRequestDto.PhoneNo == "") {
-      this.toast.warning("Please enter Phone Number", "Warning!!", { progressBar: true });
-      return;
-    }
-
-    if (this.oTransportRequestDto.StartTime == "") {
-      this.toast.warning("Please Select Time", "Warning!!", { progressBar: true });
-      return;
-    }
-    if (this.oTransportRequestDto.EndTime == "") {
-      this.toast.warning("Please Select Time", "Warning!!", { progressBar: true });
-      return;
-    }
-    if (this.oTransportRequestDto.Latitude == "") {
-      this.toast.warning("Please enter lat", "Warning!!", { progressBar: true });
-      return;
-    }
-    if (this.oTransportRequestDto.Longitude == "") {
-      this.toast.warning("Please enter long", "Warning!!", { progressBar: true });
-      return;
-    }
-    debugger
     let currentUser = CommonHelper.GetUser();
     this.oTransportRequestDto.ServiceId = Number(this.oTransportFilterDto.ServiceId);
     this.oTransportRequestDto.FileId = Number(this.oTransportRequestDto.FileId);
     this.oTransportRequestDto.StartTime = CommonHelper.formatTime(this.oTransportRequestDto.StartTime);
     this.oTransportRequestDto.EndTime = CommonHelper.formatTime(this.oTransportRequestDto.EndTime);
     this.oTransportRequestDto.IsActive = CommonHelper.booleanConvert(this.oTransportRequestDto.IsActive);
-   
+
     // After the hash is generated, proceed with the API call
     this.http.Post(`Transport/InsertTransport`, this.oTransportRequestDto).subscribe(
       (res: any) => {
@@ -288,28 +221,6 @@ export class TransportComponent implements OnInit {
       this.toast.warning("Please enter name", "Warning!!", { progressBar: true });
       return;
     }
-    if (this.oTransportRequestDto.PhoneNo == "") {
-      this.toast.warning("Please enter Phone Number", "Warning!!", { progressBar: true });
-      return;
-    }
-
-    if (this.oTransportRequestDto.StartTime == "") {
-      this.toast.warning("Please Select Time", "Warning!!", { progressBar: true });
-      return;
-    }
-    if (this.oTransportRequestDto.EndTime == "") {
-      this.toast.warning("Please Select Time", "Warning!!", { progressBar: true });
-      return;
-    }
-    if (this.oTransportRequestDto.Latitude == "") {
-      this.toast.warning("Please enter lat", "Warning!!", { progressBar: true });
-      return;
-    }
-    if (this.oTransportRequestDto.Longitude == "") {
-      this.toast.warning("Please enter long", "Warning!!", { progressBar: true });
-      return;
-    }
-    
     let currentUser = CommonHelper.GetUser();
     this.oTransportRequestDto.ServiceId = Number(this.oTransportFilterDto.ServiceId);
     this.oTransportRequestDto.FileId = Number(this.oTransportRequestDto.FileId);
@@ -366,10 +277,16 @@ export class TransportComponent implements OnInit {
     this.oTransportRequestDto.FileId = Number(getSelectedItem.FileId);
     this.oTransportRequestDto.PhoneNo = getSelectedItem.PhoneNo;
     this.oTransportRequestDto.Description = getSelectedItem.Description;
+    this.oTransportRequestDto.Driver = getSelectedItem.Driver;
+    this.oTransportRequestDto.Model = getSelectedItem.Model;
+    this.oTransportRequestDto.Cost = getSelectedItem.Cost;
+    this.oTransportRequestDto.Address = getSelectedItem.Address;
     this.oTransportRequestDto.StartTime = CommonHelper.formatTime(getSelectedItem.StartTime);
     this.oTransportRequestDto.EndTime = CommonHelper.formatTime(getSelectedItem.EndTime);
     this.oTransportRequestDto.Latitude = getSelectedItem.Latitude;
     this.oTransportRequestDto.Longitude = getSelectedItem.Longitude;
+    this.oTransportRequestDto.StartingPoint = getSelectedItem.StartingPoint;
+    this.oTransportRequestDto.EndingPoint = getSelectedItem.EndingPoint;
     this.oTransportRequestDto.IsActive = CommonHelper.booleanConvert(getSelectedItem.IsActive);
     this.oTransportRequestDto.Remarks = getSelectedItem.Remarks;
     CommonHelper.CommonButtonClick("openCommonModel");
@@ -381,16 +298,22 @@ export class TransportComponent implements OnInit {
       this.toast.warning("Please select an item", "Warning!!", { progressBar: true })
     }
 
-       this.transportId = Number(getSelectedItem.Id);
-    this.oTransportRequestDto.Name = getSelectedItem.Name;
+    this.transportId = Number(getSelectedItem.Id);
+   this.oTransportRequestDto.Name = getSelectedItem.Name;
     this.oTransportRequestDto.ServiceId = Number(getSelectedItem.ServiceId);
     this.oTransportRequestDto.FileId = Number(getSelectedItem.FileId);
     this.oTransportRequestDto.PhoneNo = getSelectedItem.PhoneNo;
     this.oTransportRequestDto.Description = getSelectedItem.Description;
+    this.oTransportRequestDto.Driver = getSelectedItem.Driver;
+    this.oTransportRequestDto.Model = getSelectedItem.Model;
+    this.oTransportRequestDto.Cost = getSelectedItem.Cost;
+    this.oTransportRequestDto.Address = getSelectedItem.Address;
     this.oTransportRequestDto.StartTime = CommonHelper.formatTime(getSelectedItem.StartTime);
     this.oTransportRequestDto.EndTime = CommonHelper.formatTime(getSelectedItem.EndTime);
     this.oTransportRequestDto.Latitude = getSelectedItem.Latitude;
     this.oTransportRequestDto.Longitude = getSelectedItem.Longitude;
+    this.oTransportRequestDto.StartingPoint = getSelectedItem.StartingPoint;
+    this.oTransportRequestDto.EndingPoint = getSelectedItem.EndingPoint;
     this.oTransportRequestDto.IsActive = CommonHelper.booleanConvert(getSelectedItem.IsActive);
     this.oTransportRequestDto.Remarks = getSelectedItem.Remarks;
     CommonHelper.CommonButtonClick("openCommonDelete");
@@ -405,4 +328,3 @@ export class TransportComponent implements OnInit {
 
 
 }
-
